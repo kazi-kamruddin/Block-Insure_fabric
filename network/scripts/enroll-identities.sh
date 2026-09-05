15408c888d23d6e161c08ef217b71b7e2b200af9
kazi-kamruddin <kazikamruddinahmed@gmail.com>
2026-09-05 18:26:04 +0600

    building Fabric CA (#2)

--- network/scripts/enroll-identities.sh
+++ network/scripts/enroll-identities.sh
@@ -36,7 +36,7 @@ enroll_peer_org() {
   cp "${ca_cert}" "${base}/ca/ca.${domain}-cert.pem"
   cp "${ca_cert}" "${base}/tlsca/tlsca.${domain}-cert.pem"
   fabric-ca-client register --caname "${ca_name}" --id.name peer0 --id.secret peer0pw --id.type peer --tls.certfiles "${ca_cert}"
-  fabric-ca-client register --caname "${ca_name}" --id.name admin --id.secret adminpw --id.type admin --tls.certfiles "${ca_cert}"
+  fabric-ca-client register --caname "${ca_name}" --id.name orgadmin --id.secret orgadminpw --id.type admin --tls.certfiles "${ca_cert}"
   fabric-ca-client register --caname "${ca_name}" --id.name "${role}" --id.secret "${role}pw" --id.type client --id.attrs "role=${role}:ecert" --tls.certfiles "${ca_cert}"
   fabric-ca-client enroll -u "https://peer0:peer0pw@localhost:${port}" --caname "${ca_name}" -M "${base}/peers/peer0.${domain}/msp" --tls.certfiles "${ca_cert}"
   cp "${base}/msp/config.yaml" "${base}/peers/peer0.${domain}/msp/config.yaml"
@@ -44,11 +44,39 @@ enroll_peer_org() {
   cp "${base}/peers/peer0.${domain}/tls/tlscacerts/"* "${base}/peers/peer0.${domain}/tls/ca.crt"
   cp "${base}/peers/peer0.${domain}/tls/signcerts/"* "${base}/peers/peer0.${domain}/tls/server.crt"
   cp "${base}/peers/peer0.${domain}/tls/keystore/"* "${base}/peers/peer0.${domain}/tls/server.key"
-  fabric-ca-client enroll -u "https://admin:adminpw@localhost:${port}" --caname "${ca_name}" -M "${base}/users/Admin@${domain}/msp" --tls.certfiles "${ca_cert}"
+  fabric-ca-client enroll -u "https://orgadmin:orgadminpw@localhost:${port}" --caname "${ca_name}" -M "${base}/users/Admin@${domain}/msp" --tls.certfiles "${ca_cert}"
   cp "${base}/msp/config.yaml" "${base}/users/Admin@${domain}/msp/config.yaml"
 }
 
 enroll_peer_org insurer InsurerMSP 7054 insurerAdmin
 enroll_peer_org hospital HospitalMSP 8054 hospitalOfficer
 enroll_peer_org auditor AuditorMSP 9054 auditor
-enroll_peer_org bank BankMSP 10054 bankOfficer
+enroll_peer_org bank BankMSP 12054 bankOfficer
+
+enroll_orderer() {
+  local domain="blockinsure.test"
+  local base="${org_root}/ordererOrganizations/${domain}"
+  local ca_cert="${org_root}/fabric-ca/orderer/ca-cert.pem"
+  export FABRIC_CA_CLIENT_HOME="${base}"
+  fabric-ca-client enroll -u https://admin:adminpw@localhost:11054 --caname ca-orderer --tls.certfiles "${ca_cert}"
+  write_node_ous "${base}/msp" "cacerts/localhost-11054-ca-orderer.pem"
+  mkdir -p "${base}/msp/tlscacerts" "${base}/tlsca"
+  cp "${ca_cert}" "${base}/msp/tlscacerts/ca.crt"
+  cp "${ca_cert}" "${base}/tlsca/tlsca.${domain}-cert.pem"
+  fabric-ca-client register --caname ca-orderer --id.name orderer --id.secret ordererpw --id.type orderer --tls.certfiles "${ca_cert}"
+  fabric-ca-client register --caname ca-orderer --id.name ordereradmin --id.secret ordereradminpw --id.type admin --tls.certfiles "${ca_cert}"
+  fabric-ca-client enroll -u https://orderer:ordererpw@localhost:11054 --caname ca-orderer -M "${base}/orderers/orderer.${domain}/msp" --tls.certfiles "${ca_cert}"
+  cp "${base}/msp/config.yaml" "${base}/orderers/orderer.${domain}/msp/config.yaml"
+  fabric-ca-client enroll -u https://orderer:ordererpw@localhost:11054 --caname ca-orderer -M "${base}/orderers/orderer.${domain}/tls" --enrollment.profile tls --csr.hosts "orderer.${domain}" --csr.hosts localhost --tls.certfiles "${ca_cert}"
+  cp "${base}/orderers/orderer.${domain}/tls/tlscacerts/"* "${base}/orderers/orderer.${domain}/tls/ca.crt"
+  cp "${base}/orderers/orderer.${domain}/tls/signcerts/"* "${base}/orderers/orderer.${domain}/tls/server.crt"
+  cp "${base}/orderers/orderer.${domain}/tls/keystore/"* "${base}/orderers/orderer.${domain}/tls/server.key"
+  fabric-ca-client enroll -u https://ordereradmin:ordereradminpw@localhost:11054 --caname ca-orderer -M "${base}/users/Admin@${domain}/msp" --tls.certfiles "${ca_cert}"
+  cp "${base}/msp/config.yaml" "${base}/users/Admin@${domain}/msp/config.yaml"
+  fabric-ca-client enroll -u https://ordereradmin:ordereradminpw@localhost:11054 --caname ca-orderer -M "${base}/users/Admin@${domain}/tls" --enrollment.profile tls --csr.hosts "admin.${domain}" --csr.hosts localhost --tls.certfiles "${ca_cert}"
+  cp "${base}/users/Admin@${domain}/tls/tlscacerts/"* "${base}/users/Admin@${domain}/tls/ca.crt"
+  cp "${base}/users/Admin@${domain}/tls/signcerts/"* "${base}/users/Admin@${domain}/tls/client.crt"
+  cp "${base}/users/Admin@${domain}/tls/keystore/"* "${base}/users/Admin@${domain}/tls/client.key"
+}
+
+enroll_orderer
